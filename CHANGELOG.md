4621fdcc0a49089404f24dac0b5af661c4e26d9d
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-09-15 15:38:22 +0200

    feat: Initial commit with default sample project

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+<!-- Keep a Changelog guide -> https://keepachangelog.com -->
+
+# FilterOutPreviewUsages Changelog
+
+## [Unreleased]
---
+++ backend/src/main/kotlin/BackendRpcApiProvider.kt
@@ -0,0 +1,12 @@
+package com.infomaniak
+
+import com.intellij.platform.rpc.backend.RemoteApiProvider
+import fleet.rpc.remoteApiDescriptor
+
+internal class BackendRpcApiProvider : RemoteApiProvider {
+    override fun RemoteApiProvider.Sink.remoteApis() {
+        remoteApi(remoteApiDescriptor<ChatRepositoryRpcApi>()) {
+            BackendChatRepositoryRpcApi()
+        }
+    }
+}
